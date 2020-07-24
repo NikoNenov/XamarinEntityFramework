d3c2768a02abebcc6b021a07aa785b44cb1eeb45
Nikolay Nenov <nenovn@curtisinst.com>
2020-07-24 08:41:25 +0200

    Initial commit

---
+++ XamarinEntityFramework/XamarinEntityFramework/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Prism.Navigation;
+
+namespace XamarinEntityFramework.ViewModels
+{
+  public class MainPageViewModel : BaseViewModel
+  {
+    public MainPageViewModel(INavigationService navigationService)
+        : base(navigationService)
+    {
+      Title = "Main Page";
+    }
+  }
+}
